9f42dc68c5f8b8676ced700f55fb4a47b17d4404
masaaki stephane <62558026+Stephane226@users.noreply.github.com>
2020-10-20 03:33:50 +0300

    Mise en place des listes

---
+++ script.js
@@ -0,0 +1,22 @@
+// Debut de TodoList
+//Declaration Des Variablesva
+var TodoTitle = document.getElementById("inp");
+var TodoDate = document.getElementById("TdDate");
+var Sub = document.getElementById("AddTD");
+
+//
+Sub.addEventListener("click", function(){
+   alert(TodoDate.value)
+})
+
+
+
+
+
+
+
+
+
+
+
+//Fin de Todo List
